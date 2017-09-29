25ee37d3bbbbf77c593bf20cafa6bdd2da90222f
LikoLi <173763111@qq.com>
2017-09-29 23:39:18 +0800

    init

---
+++ src/main/java/org/liko/framework/log/App.java
@@ -0,0 +1,13 @@
+package org.liko.framework.log;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
